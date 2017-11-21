ea9073fa581d70653546325e1c582fd1db751b6a
Ernestas Poskus <ernestas.poskus@gmail.com>
2017-11-21 23:18:47 +0200

    Bump to iron 0.6.0 and maud 0.17.2

--- src/middlewares/validation.rs
+++ src/middlewares/validation.rs
@@ -7,7 +7,7 @@ pub struct ValidationMiddleware;
 
 impl AfterMiddleware for ValidationMiddleware {
     fn catch(&self, _: &mut Request, err: IronError) -> IronResult<Response> {
-        if let Some(e) = err.error.downcast::<ValidationError>() {
+        if let Some(e) = err.error.downcast_ref::<ValidationError>() {
             return Ok(err.response.set(e.to_string()));
         }
         Err(err)
