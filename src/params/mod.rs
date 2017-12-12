78852f92c0721ac1987aeef151018d898bdc5895
Ernestas Poskus <ernestas.poskus@gmail.com>
2017-12-12 19:23:32 +0200

    Rename StructParams to SP

--- src/params/mod.rs
+++ src/params/mod.rs
@@ -1,14 +1,14 @@
-/// Type must implement this trait to get validated StructParams
+/// Type must implement this trait to get validated SP
 /// RE - database connection acquire, IO, parsing errors
 /// E - struct/map errors for user to display
 /// T - underlying type after validation
-pub trait ValidateStructParams<T, E, RE> {
+pub trait ValidateSP<T, E, RE> {
     /// Either returns validated define type or expected errors
-    fn validate(self) -> Result<StructParams<T, E>, RE>;
+    fn validate(self) -> Result<SP<T, E>, RE>;
 }
 
-/// Returns valid/invalid params
-pub enum StructParams<T, E> {
+/// Returns struct params valid/invalid params
+pub enum SP<T, E> {
     /// Valid type
     Valid(T),
     /// Invalid type
