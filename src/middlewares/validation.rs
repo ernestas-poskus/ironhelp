36f88886c95bc9651ada94c5cc3e98966cc031fd
Ernestas Poskus <ernestas.poskus@gmail.com>
2018-01-21 23:24:16 +0200

    Return BadRequest in catch After ValidationMiddleware

--- src/middlewares/validation.rs
+++ src/middlewares/validation.rs
@@ -1,4 +1,4 @@
-use iron::{AfterMiddleware, IronResult, Request, Response, IronError, Set};
+use iron::{AfterMiddleware, IronResult, Request, Response, IronError, status};
 use validation::ValidationError;
 
 /// ValidationMiddleware responds with BadRequest and serialized
@@ -8,7 +8,7 @@ pub struct ValidationMiddleware;
 impl AfterMiddleware for ValidationMiddleware {
     fn catch(&self, _: &mut Request, err: IronError) -> IronResult<Response> {
         if let Some(e) = err.error.downcast_ref::<ValidationError>() {
-            return Ok(err.response.set(e.to_string()));
+            return Ok(Response::with((status::BadRequest, e.to_string())));
         }
         Err(err)
     }
