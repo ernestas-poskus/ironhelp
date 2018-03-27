d85a382d961f4753253c95528655948141b38613
Ernestas Poskus <ernestas.poskus@gmail.com>
2018-03-27 20:27:43 +0300

    Remove ValidationMiddleware

--- src/middlewares/mod.rs
+++ src/middlewares/mod.rs
@@ -1,7 +1,6 @@
 mod json;
 mod html;
 mod session;
-mod validation;
 mod status;
 mod logger;
 mod access_control_allow_origin;
@@ -10,7 +9,6 @@ mod request_body;
 pub use self::json::Json;
 pub use self::html::Html;
 pub use self::session::{CookiesSession, Session};
-pub use self::validation::ValidationMiddleware;
 pub use self::status::StatusCatchMiddleware;
 pub use self::logger::LoggerMiddleware;
 pub use self::access_control_allow_origin::AccessControlAllowOriginMiddleware;
